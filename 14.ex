191fc3dd699d25e2d30d69ccd9b52c4fd86e1c36
Ayoush Chourasia <hireayoush@gmail.com>
2024-04-15 14:27:04 +0530

    Todays 2 questions

--- 14.ex
+++ 14.ex
@@ -1,5 +1,12 @@
-defmodule Solution do
+defmodule CommonPrefix do
   def longest_common_prefix(strs) do
 
+    strs
+    |> Enum.map(&String.to_charlist/1)
+    |> Enum.zip()
+    |> Enum.map(&Tuple.to_list/1)
+    |> Enum.map(&Enum.uniq/1)
+    |> Enum.take_while(&match?([_], &1))
+    |> IO.iodata_to_binary()
   end
 end
